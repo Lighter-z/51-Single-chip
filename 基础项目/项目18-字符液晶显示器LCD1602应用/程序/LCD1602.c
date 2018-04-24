74bbebea655c6752d40be10b4a12f479dbe9d04e
zhao ming <1106606588@qq.com>
2018-04-24 19:16:22 +0800

    第二次
    添加注释

--- 基础项目/项目18-字符液晶显示器LCD1602应用/程序/LCD1602.c
+++ 基础项目/项目18-字符液晶显示器LCD1602应用/程序/LCD1602.c
@@ -6,52 +6,58 @@ void delay(uint x)
 {
 	while(x--);	
 } 
-
+/***向LCD写一个命令**/
 void write_command(uchar command)
 {
 	RW = 0;
 	RS = 0;
 	P0 = command;
 	E = 1;
-	delay(100);
+	delay(100);//等待接收，忽略读状态操作
 	E = 0;
 	RW = 1;
 } 
+/***向LCD写一个数据**/
 void write_data(uchar date)
 {
 	RW = 0;
 	RS = 1;
 	P0 = date;
 	E = 1;
-	delay(100);
+	delay(100);//等待接收
 	E = 0;
 	RW = 1;
 } 
+/**初始化**/
 void FLCD1602_init(void)
 {
-	write_command(0x38);
-	write_command(0x0f);
-	//write_command(0x0c);
-	write_command(0x06);
+	write_command(0x38);//两行，每行16个字符，每个字符是5*7点阵
+	write_command(0x0f);//光标显示并闪烁
+	//write_command(0x0c);//光标不显示
+	write_command(0x06);//光标随字符右移
 }  
+/**清屏**/
 void FLCD1602_clear()
 {
 	write_command(0x01);
 	write_command(0x02);
 }
+/**向LCD写字符串**/
 void display_string(uchar *p)
 {
-	while(*p)
+	while(*p)//字符非空
 	{
-		write_data(*p);
-		p++;
+		write_data(*p);//写字符
+		p++;             //数据指针++
 	}
 }
+/**向LCD写数字变量**/
 void display_num(uchar x)
 {
 	write_data(num[x / 10]);
 	write_data(num[x % 10]);
 }
+/**定位**/
 void gotoxy(uchar y,uchar x)
 {
 	if(y == 1)
